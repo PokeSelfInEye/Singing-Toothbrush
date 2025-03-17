9d5fd9f8e712dcfc72ffb0f7ec18383be296f7d4
Nico Polcaro <38696804+PokeSelfInEye@users.noreply.github.com>
2025-03-17 14:15:46 -0400

    Add files via upload

--- Singing_toothbrush_scratch_file.ino
+++ Singing_toothbrush_scratch_file.ino
@@ -3,7 +3,6 @@
 
 //pin variables
 int const motorPin1 = 7; //digital motor control pins
-int const motorPin2 = 8;
 int const aReadPin1 = 14; //analog read pin
 int const aWritePin1 = 6;
 
@@ -13,8 +12,6 @@ int frequencyDelayMs = 1; //the delay in milliseconds to give the frequency
 int frequencyDelayUs = 1000; //the delay in microseconds to make a frequency
 
 //not frequency variables
-float const noteC3 = 130.813; //C3
-float const noteD3 = 146.832; //D3
 float const noteC4 = 261.262; //C4
 float const noteD4 = 293.665; //D4
 float const noteE4 = 329.628; //E4
@@ -23,16 +20,15 @@ float const noteG4 = 391.995; //G4
 float const noteA4 = 440.000; //A4
 float const noteB4 = 493.883; //B4
 
-int const beatsPerMinute = 80;
+int const beatsPerMinute = 120;
 int beatDurationMs;
 
+MIDI_CREATE_DEFAULT_INSTANCE();
+
 void setup() {
-  Serial.begin(9600);
+  MIDI.begin(MIDI_CHANNEL_OMNI);
   pinMode(motorPin1, OUTPUT);
-  pinMode(motorPin2, OUTPUT);
-  digitalWrite(motorPin1, HIGH);
-  digitalWrite(motorPin2, LOW);
-  beatDurationMs = 60000/beatsPerMinute;
+  //beatDurationMs = 60000/beatsPerMinute;
 }
 
 void loop() {
@@ -46,21 +42,21 @@ void loop() {
   potInput = analogRead(aReadPin1);
   frequencyDelayUs = (potInput*4) + 1000;*/
   playNote(noteE4, beatDurationMs);
-  playNote(noteD3, beatDurationMs);
-  playNote(noteC3, beatDurationMs);
-  playNote(noteD3, beatDurationMs);
+  playNote(noteD4, beatDurationMs);
+  playNote(noteC4, beatDurationMs);
+  playNote(noteD4, beatDurationMs);
   
   playNote(noteE4, beatDurationMs);
-  playNote(noteE4, beatDurationMs);
-  playNote(noteE4, beatDurationMs*2);
-  
-  playNote(noteD3, beatDurationMs);
-  playNote(noteD3, beatDurationMs);
-  playNote(noteD3, beatDurationMs*2);
-
-  playNote(noteE4, beatDurationMs);
-  playNote(noteG4, beatDurationMs);
-  playNote(noteG4, beatDurationMs*2);
+  playNote(noteE4, beatDurationMs);   
+  playNote(noteE4, beatDurationMs*2); 
+                                       
+  playNote(noteD4, beatDurationMs);   
+  playNote(noteD4, beatDurationMs);   
+  playNote(noteD4, beatDurationMs*2); 
+                                      
+  playNote(noteE4, beatDurationMs);  
+  playNote(noteG4, beatDurationMs);    
+  playNote(noteG4, beatDurationMs*2); 
   
   
   
@@ -71,13 +67,11 @@ void playNote(float noteFrequencyHz,int noteDurationMs) {
   int timerStart = millis();
   int timerTracker = millis();
   Serial.println(noteFrequencyHz);
-  while (timerTracker - timerStart < 1000) {
+  while (timerTracker - timerStart < noteDurationMs) {
     timerTracker = millis();
     digitalWrite(motorPin1, LOW);
-    digitalWrite(motorPin2, HIGH); //DIGITAL ALTERNATING PINS
     delayMicroseconds(noteDelayUs);
     digitalWrite(motorPin1, HIGH);
-    digitalWrite(motorPin2, LOW);
     //Serial.println(timerTracker-timerStart);
     delayMicroseconds(noteDelayUs);
   }
